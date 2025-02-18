0c5b85a7119f8d9db52b678264a7316d29ed96b1
Keith Bartholomew <keith.bartholomew@gmail.com>
2025-02-17 19:57:39 -0600

    redirect to most recent, non-future newsletter

--- frontend/app/newsletter/latest/route.ts
+++ frontend/app/newsletter/latest/route.ts
@@ -5,7 +5,10 @@ import { redirect } from "next/navigation";
 
 export async function GET() {
   const { url } = await draftModeClient().fetch(
-    '*[_type == "newsletter"] | order(date desc)[0]{url}'
+    `*[
+       _type == "newsletter" &&
+       date <= ${new Date().toISOString()}
+     ] | order(date desc)[0]{url}`
   );
 
   redirect(url || "/");
