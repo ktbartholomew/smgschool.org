7a9d45086334cfdd80162d496d86b8290ddc34f5
Keith Bartholomew <keith.bartholomew@gmail.com>
2025-08-11 11:35:11 -0500

    revalidate newsletter/latest route

--- frontend/app/newsletter/latest/route.ts
+++ frontend/app/newsletter/latest/route.ts
@@ -3,6 +3,8 @@
 import { draftModeClient } from "@/lib/sanity/draft-mode-client";
 import { redirect } from "next/navigation";
 
+export const revalidate = 60;
+
 export async function GET() {
   const { url } = await draftModeClient().fetch(
     `*[
